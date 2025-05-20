4d514a98b5e01c7c070c03decca560a32759dc75
Zizi-ll <2933998661@qq.com>
2025-05-20 23:36:59 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,28 @@
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_food.models import db, User
+from flask_food.routes import bp
+from flask_food import models
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:linyanzi2005@localhost:3307/mydatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+app.register_blueprint(bp)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
