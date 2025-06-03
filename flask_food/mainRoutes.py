fec0096444be8a6fcf82d50b2409f706daf5ac7f
Grammar0201 <2670765754@qq.com>
2025-06-04 01:13:30 +0800

    登录/注册/首页
    实现了登录功能，注册前端（和数据库的连接还有点问题），登陆后进入browser

--- flask_food/mainRoutes.py
+++ flask_food/mainRoutes.py
@@ -1,17 +1,13 @@
 # flask_food/mainRoutes.py
 
-from flask import Blueprint, render_template, redirect, url_for
-from flask_login import login_required, current_user
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 @main_bp.route('/index')
-@login_required
 def index():
     """
-    主页 - 需要登录才能访问
+    主页 - 显示欢迎界面
     """
-    if not current_user.is_authenticated:
-        return redirect(url_for('auth.login'))
     return render_template('index.html', title='欢迎')
